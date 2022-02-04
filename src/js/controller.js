53219c9c2994d285feebbdf1fd585ab8046b1baa
AaronTran206 <aarontran.wa@gmail.com>
2022-02-03 19:12:48 -0700

    added new feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -124,6 +124,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const welcome = function () {
+  console.log('Welcome to the application! :)');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -132,7 +136,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  welcome();
 };
 init();
-
-console.log('Welcome to the application! :)');
